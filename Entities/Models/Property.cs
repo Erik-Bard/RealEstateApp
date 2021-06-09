00cdb7c4c097fa3d31b2bb7b7380c94453c8a518
SinaSultani <sinasultani92@gmail.com>
2021-06-09 17:12:46 +0200

    Added validation on PropertyModel and Advertisment Model

--- Entities/Models/Property.cs
+++ Entities/Models/Property.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace Entities.Models
     public class Property
     {
         public Guid Id { get; set; }
+        [Required(ErrorMessage = "A year of construction is required for this property.")]
         public int YearOfConstruction { get; set; }
+        [Required(ErrorMessage = "An address is required for this property.")]
         public string Address { get; set; }
     }
 }
