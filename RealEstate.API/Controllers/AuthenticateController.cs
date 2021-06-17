7ef25c116378452952ddecda3e541274a9bfe4f2
Erik Bard <erik.bard@outlook.com>
2021-06-17 16:01:03 +0200

    error debug handler

--- RealEstate.API/Controllers/AuthenticateController.cs
+++ RealEstate.API/Controllers/AuthenticateController.cs
@@ -87,7 +87,10 @@ namespace RealEstate.API.Controllers
 
             var userExists = await userManager.FindByNameAsync(model.Username);
             if (userExists != null)
-                return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
+            {
+                _logger.Error("Tried to create a duplicate User!");
+                return BadRequest("Error, User already exists!");
+            }
 
             ApplicationUser user = new ApplicationUser()
             {
